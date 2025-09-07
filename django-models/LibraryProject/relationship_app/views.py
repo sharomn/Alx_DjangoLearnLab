4f670858721964095371559dcb0440b2e4ddc833
sharon namatovu <laykadaniels@gmail.com>
2025-09-07 18:33:22 +0400

    this is library

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -11,5 +11,5 @@ from .models import Library
 
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'library_detail.html'
+    template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
