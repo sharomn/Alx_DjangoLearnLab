1dc2eef3b2708ca1335ef0400aa693855cbfd07d
sharon namatovu <laykadaniels@gmail.com>
2025-09-07 18:29:44 +0400

    keep fixing those small typo

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -3,7 +3,7 @@ from .models import Book
 
 def list_books(request):
     books = Book.objects.all()
-    return render(request, 'relationship/list_books.html', {'books': books})
+    return render(request, 'relationship_app/list_books.html', {'books': books})
 # Create your views here.
 
 from django.views.generic.detail import DetailView
