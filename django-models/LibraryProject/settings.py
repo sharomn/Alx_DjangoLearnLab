638b8d264ca9e0b7e2b22ca6b822dd7438174a84
sharon namatovu <laykadaniels@gmail.com>
2025-09-09 14:09:45 +0400

    add templates in the installed apps

--- django-models/LibraryProject/settings.py
+++ django-models/LibraryProject/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'bookshelf',
     'relationship_app',
+    'templates',
 ]
 
 MIDDLEWARE = [
@@ -64,8 +65,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'list_books.html',
-                'library_detail.html',
+                
+            
             ],
         },
     },
