55636a5497d7cfb2de84cc9444432d65157e8392
sharon namatovu <laykadaniels@gmail.com>
2025-09-09 13:40:54 +0400

    add these html files in the right folder

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -9,5 +9,9 @@ urlpatterns = [
     # ✅ Checker-required patterns
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
-    path('register/', views.register, name='register'),  # ✅ Checker wants "views.register"
+    path('register/', views.register, name='register'), 
+    path('admin/', views.admin_view, name='admin_view'),
+    path('librarian/', views.librarian_view, name='librarian_view'),
+    path('member/', views.member_view, name='member_view'), # ✅ Checker wants "views.register"
 ]
+
