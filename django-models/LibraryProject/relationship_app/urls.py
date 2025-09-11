b7422d93d30a119b846be290b81abac50d27ebeb
sharon namatovu <laykadaniels@gmail.com>
2025-09-11 15:48:09 +0400

    add_book/

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -14,8 +14,8 @@ urlpatterns = [
     path('admin/', views.admin_view, name='admin_view'),
     path('librarian/', views.librarian_view, name='librarian_view'),
     path('member/', views.member_view, name='member_view'),
-    path('books/add/', views.add_book, name='add_book'),
-    path('books/<int:pk>/edit/', views.edit_book, name='edit_book'),
+    path('books/add/', views.add_book, name='add_book/'),
+    path('books/<int:pk>/edit/', views.edit_book, name='edit_book/'),
     path('books/<int:pk>/delete/', views.delete_book, name='delete_book'), # ✅ Checker wants "views.register"
 ]
 
