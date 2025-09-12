221c93a03f457eede9d958353e34a8c7dd111a6f
sharon namatovu <laykadaniels@gmail.com>
2025-09-12 12:28:14 +0400

    set models.py

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -36,6 +36,21 @@ class CustomUser(AbstractUser):
     def __str__(self):
         return self.username
     
+
+# bookshelf/models.py
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+
+    class Meta:
+        permissions = [
+            ("can_view", "Can view book"),
+            ("can_create", "Can create book"),
+            ("can_edit", "Can edit book"),
+            ("can_delete", "Can delete book"),
+        ]
+    
     
 
 
