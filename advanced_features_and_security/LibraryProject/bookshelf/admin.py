6b3ee6147342657fcd18fb2b5a6e3b52b23d4e20
sharon namatovu <laykadaniels@gmail.com>
2025-09-12 11:21:23 +0400

    update admin.py

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -6,6 +6,18 @@ from .models import Book
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'publication_year')  # Columns shown in list view
     list_filter = ('publication_year', 'author')            # Sidebar filters
-    search_fields = ('title', 'author')                     # Search bar fields
+    search_fields = ('title', 'author') 
+    
+    
+from .models import CustomUser
+from django.contrib.auth.admin import UserAdmin
 
-admin.site.register(Book, BookAdmin)
+class CustomUserAdmin(UserAdmin):
+    # Optional: customize how fields appear in admin
+    model = CustomUser
+    list_display = ['username', 'email', 'is_staff', 'is_active']
+    search_fields = ['username', 'email']
+    ordering = ['username']
+                   # Search bar fields
+
+admin.site.register(Book, BookAdmin, CustomUser, CustomUserAdmin)
