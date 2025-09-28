7c7ae9f1974ec7a5383487108e4721bacc5224b0
sharon namatovu <laykadaniels@gmail.com>
2025-09-28 22:44:55 +0400

    add al apps in the right files

--- advanced_api_project/api/urls.py
+++ advanced_api_project/api/urls.py
@@ -1,8 +1,6 @@
-from django.urls import path, include
+from django.urls import path
 from .views import BookListCreateView
 
 urlpatterns = [
     path('books/', BookListCreateView.as_view(), name='book-list-create'),
-    path('api/', include('api.urls')),
-
 ]
