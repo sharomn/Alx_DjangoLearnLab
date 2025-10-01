3a183ef2d71e47994f4fa1182e05320978a402f2
sharon namatovu <laykadaniels@gmail.com>
2025-10-02 01:01:36 +0400

    ensure login profiles

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,9 +1,19 @@
 from django.urls import path
 from . import views
+from .views import (
+    PostListView, PostDetailView, PostCreateView,
+    PostUpdateView, PostDeleteView
+)
 
 urlpatterns = [
     path('register/', views.register, name='register'),
     path('profile/', views.profile, name='profile'),
     path('login/',views.login, name='login'),
     path('logout/',views.logout,name='logout'),
+
+    path('posts/', PostListView.as_view(), name='post-list'),
+    path('posts/new/', PostCreateView.as_view(), name='post-create'),
+    path('posts/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('posts/<int:pk>/edit/', PostUpdateView.as_view(), name='post-edit'),
+    path('posts/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
 ]
