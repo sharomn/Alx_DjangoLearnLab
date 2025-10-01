eb29ac0824dd7d5aa4ad20a3bcfdca7ca262b08d
sharon namatovu <laykadaniels@gmail.com>
2025-10-01 23:44:06 +0400

    yes add all files

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -77,6 +77,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'USER': '',
+        'PORT': '',
+
          
      
     }
